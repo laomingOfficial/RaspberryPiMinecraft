d41b74a94df98bf7f47964f19631ff72a16f0cc6
Marc Tönsing <mtoensing@users.noreply.github.com>
2019-09-16 19:43:58 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # James Chambers - V1.0 - March 24th 2018
-# Marc Tönsing - V1.1 - May 18th 2018
+# Marc Tönsing - V1.2 - September 16th 2019
 # Minecraft Server startup script using screen
 echo "Starting Minecraft server.  To view window type screen -r minecraft."
 echo "To minimize the window and let the server run in the background, press Ctrl+A then Ctrl+D"
